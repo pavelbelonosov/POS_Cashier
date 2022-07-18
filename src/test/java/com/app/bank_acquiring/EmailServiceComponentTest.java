302ac17d7f3046416a17e0018ee51083cae17aa6
pablo <pablo11grande@gmail.com>
2022-07-19 00:06:16 +0500

    Added EmailServiceComponentTest

---
+++ src/test/java/com/app/bank_acquiring/EmailServiceComponentTest.java
@@ -0,0 +1,36 @@
+package com.app.bank_acquiring;
+
+import com.app.bank_acquiring.service.EmailServiceComponent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailServiceComponentTest {
+
+    @Autowired
+    private EmailServiceComponent emailServiceComponent;
+
+    @Test
+    public void givenCorrectEmailAddress_whenSendMail_thenThrowNoException() {
+        try {
+            emailServiceComponent.sendMail("pablo11grande@gmail.com", "cheque");
+        } catch (MailException e) {
+            fail();
+        }
+    }
+
+    @Test(expected = MailException.class)
+    public void givenIncorrectEmailAddress_whenSendMail_thenThrowException() {
+        emailServiceComponent.sendMail("pablo11grandegmail.com", "cheque");
+    }
+}
