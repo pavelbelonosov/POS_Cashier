aa9e5288e4f543a252b0d7c523d1c627fea63df6
pablo <pablo11grande@gmail.com>
2022-07-27 01:49:29 +0500

    refactored transaction entity(equals and hashcode now use  generated id from superclass, added hibernate @Type (text) annotation to @Lob field in order to prevent Lob stream failing).

--- src/main/java/com/app/bank_acquiring/domain/transaction/Transaction.java
+++ src/main/java/com/app/bank_acquiring/domain/transaction/Transaction.java
@@ -2,36 +2,50 @@ package com.app.bank_acquiring.domain.transaction;
 
 import com.app.bank_acquiring.domain.Terminal;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaction extends AbstractPersistable<Long> {
 
     @NotNull
     private Boolean status;
+
     private LocalDateTime dateTime;
 
     @Enumerated(EnumType.STRING)
     private Type type;
+
     private double amount;
+
     @Lob
+    @org.hibernate.annotations.Type(type = "org.hibernate.type.TextType")
     private String cheque = "";
+
     private String cashier;
 
     @ManyToOne
     @JsonIgnore
     private Terminal terminal;
+
+    @Override
+    public void setId(Long id) {
+        if (id != null) super.setId(id);
+    }
+
+    @Override
+    @EqualsAndHashCode.Include
+    public Long getId() {
+        return super.getId();
+    }
 
 }
