24754d9c2237294445e501fb9e2b3aa4f120d926
pablo <pablo11grande@gmail.com>
2022-08-04 23:10:04 +0500

    for testing issue was added httpBasic(), but then removed

--- src/main/java/com/app/bank_acquiring/config/SecurityConfiguration.java
+++ src/main/java/com/app/bank_acquiring/config/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //enable h2-console
+
         http.csrf().disable().formLogin().permitAll();
         http.headers().frameOptions().sameOrigin();
 
