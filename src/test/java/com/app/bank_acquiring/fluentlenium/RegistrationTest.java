bf5bc0dd4e1c8a56bf0a5f6938c42f50c91f483b
pablo <pablo11grande@gmail.com>
2022-08-13 01:04:36 +0500

    added givenInvalidData_whenPost_thenReturnsErrorsFromServer

--- src/test/java/com/app/bank_acquiring/fluentlenium/RegistrationTest.java
+++ src/test/java/com/app/bank_acquiring/fluentlenium/RegistrationTest.java
@@ -1,26 +1,16 @@
 package com.app.bank_acquiring.fluentlenium;
 
-import com.app.bank_acquiring.repository.AccountRepository;
+
 import org.fluentlenium.adapter.junit.FluentTest;
-
-import static org.fluentlenium.core.filter.FilterConstructor.*;
-import static org.junit.Assert.*;
-
-import org.fluentlenium.core.FluentPage;
-import org.fluentlenium.core.annotation.Page;
-import org.fluentlenium.core.annotation.PageUrl;
-import org.fluentlenium.core.domain.FluentWebElement;
-import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.FindBy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.concurrent.TimeUnit;
 
 
@@ -29,31 +19,43 @@ import java.util.concurrent.TimeUnit;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegistrationTest extends FluentTest {
 
-    @Autowired
-    private AccountRepository accountRepository;
-
     @LocalServerPort
     private Integer port;
 
-    @Before
-    public void setUp(){
+
+    @Test
+    public void testGetAndPost() {
+        //opening registration endpoint
+        goTo("http://localhost:" + port + "/accounts/registration");
+        assertThat(window().title()).contains("POS-кассир | Регистрация");
+        //filling register form and click submit button
+        find("#username").fill().with("useruser");
+        find("#password").fill().with("useruser");
+        find("#repeatPWD").fill().with("useruser");
+        find("#email").fill().with("user@user.ru");
+        find("button").click();
+        //should redirect to login page
+        await().atMost(1, TimeUnit.SECONDS);
+        assertThat(url()).contains("/login.html");
+        assertThat(window().title()).contains("POS-кассир | Вход");
     }
 
     @Test
-    public void testGetAndPost(){
-       goTo("http://localhost:" + port + "/accounts/registration");
-       assertThat(window().title()).contains("POS-кассир | Регистрация");
-       //filling reg.form and click submit button
-       find("#username").fill().with("useruser");
-       find("#password").fill().with("useruser");
-       find("#repeatPWD").fill().with("useruser");
-       find("#email").fill().with("user@user.ru");
-       find("button").click();
-       //should redirect to login page
-       await().atMost(1, TimeUnit.SECONDS);
-       assertThat(url()).contains("/login.html");
-       assertThat(window().title()).contains("POS-кассир | Вход");
-       //new user is saved
-       assertThat(accountRepository.findByUsername("useruser").getUsername().equals("useruser"));
+    public void givenInvalidData_whenPost_thenReturnsErrorsFromServer() {
+        //opening registration endpoint
+        goTo("http://localhost:" + port + "/accounts/registration");
+        assertThat(window().title()).contains("POS-кассир | Регистрация");
+        //filling register form with invalid data and click submit button
+        find("#username").fill().with("user");//invalid name
+        find("#password").fill().with("user");//invalid pwd
+        find("#repeatPWD").fill().with("useruser");//pwds not match
+        find("#email").fill().with("user@user.ru");
+        find("button").click();
+        //no redirection to login page due validation errors
+        await().atMost(1, TimeUnit.SECONDS);
+        assertThat(url()).contains("/accounts/registration");
+        assertThat(pageSource()).contains("Пароли не совпадают");
+        assertThat(pageSource()).contains("Слишком короткий пароль");
+        assertThat(pageSource()).contains("Логин от 8 до 40 символов");
     }
 }
