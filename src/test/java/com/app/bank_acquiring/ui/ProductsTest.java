b5aae79d62cba046b3b36995043905991e8ee483
pablo <pablo11grande@gmail.com>
2022-08-21 00:41:44 +0500

    added tests for copy to shop and update balance functions

--- src/test/java/com/app/bank_acquiring/ui/ProductsTest.java
+++ src/test/java/com/app/bank_acquiring/ui/ProductsTest.java
@@ -4,6 +4,7 @@ package com.app.bank_acquiring.ui;
 import org.fluentlenium.core.domain.FluentWebElement;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.FindBy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,7 +18,7 @@ import static org.fluentlenium.core.filter.FilterConstructor.withName;
 public class ProductsTest extends BaseTest {
 
     @FindBy(css = "table")
-    private FluentWebElement prodTables;
+    private FluentWebElement prodTable;
 
     @Test
     public void whenSubmitForm_thenNewProductAddedInTable() {
@@ -27,6 +28,7 @@ public class ProductsTest extends BaseTest {
         createShop("MyShop");
         //opening products page
         goTo(productsUrl);
+        isAtProductsPage();
         //filling form with new product
         selectProdTable(1);//select 1-st existing shop (0 is empty select)
         verifyProdTableIsEmpty();
@@ -38,27 +40,28 @@ public class ProductsTest extends BaseTest {
         find("#vendorCode").fill().with("123456ABCD");
         find("#barCode").fill().with("123456789");
         find("#balance").fill().with("150");//mandatory input
-        //find("form").first().submit();
-        el("form").el("button",containingTextContent("Сохранить")).click();
+        el("form").el("button", containingTextContent("Сохранить")).click();
+        isAtProductsPage();
         //after reload page should contain new product in table
-        assertThat(prodTables.textContent()).contains(
+        assertThat(prodTable.textContent()).contains(
                 "MyProduct",
                 "Товар",
                 "100.10",
                 "123.45",
                 "123456ABCD"
         );
-        assertThat(prodTables.el("#balances").attribute("placeholder")).contains("150");
+        assertThat(prodTable.el("#balances").attribute("placeholder")).contains("150");
     }
 
     @Test
-    public void whenClickDeleteProdButton_thenProductRemovedFromTable(){
+    public void whenClick_DeleteProduct_Button_thenProductRemovedFromTable() {
         //register->login->add shop
         registerAdminUser("useruser", "password");
         loginUser("useruser", "password");
         createShop("MyShop");
         //opening products page
         goTo(productsUrl);
+        isAtProductsPage();
         //filling form with new product
         selectProdTable(1);//select 1-st existing shop (0 is empty select)
         find("#type").fillSelect().withText("Товар");//product type (item or service), mandatory
@@ -67,21 +70,89 @@ public class ProductsTest extends BaseTest {
         find("#measurementUnit").fillSelect().withIndex(0);//mandatory select
         find("#balance").fill().with("150");//mandatory input
         find("form").first().submit();
+        isAtProductsPage();
         //clicking checkbox of product to delete
-        //window().setPosition()
         selectProdTable(1);
-        prodTables.el("input",withName("prods")).click();
-        find("form").last().el("button",containingTextContent("Удалить")).click();
+        prodTable.el("input", withName("prods")).click();
+        find("form").last().el("button", containingTextContent("Удалить")).click();
+        isAtProductsPage();
         //product should be vanished from table
         selectProdTable(1);
         verifyProdTableIsEmpty();
     }
 
-    private void verifyProdTableIsEmpty() {
-        if (prodTables.present()) assertThat(prodTables.textContent()).contains("Нет добавленных товаров");
+    @Test
+    public void whenClick_UpdateBalance_Button_thenProductBalanceUpdatedInTable() {
+        //register->login->add shop
+        registerAdminUser("useruser", "password");
+        loginUser("useruser", "password");
+        createShop("MyShop");
+        //opening products page
+        goTo(productsUrl);
+        isAtProductsPage();
+        //filling form with new product
+        selectProdTable(1);//select 1-st existing shop (0 is empty select)
+        find("#type").fillSelect().withText("Товар");//product type (item or service), mandatory
+        find("#name").fill().with("MyProduct");//mandatory input
+        find("#sellingPrice").fill().with("123.45");//mandatory input
+        find("#measurementUnit").fillSelect().withIndex(0);//mandatory select
+        find("#balance").fill().with("150");//mandatory input
+        find("form").first().submit();
+        isAtProductsPage();
+        //updating prod balance(checkbox+input new value+button)
+        selectProdTable(1);
+        prodTable.el("input", withName("prods")).click();//clicking checkbox
+        prodTable.el("input", withName("balances")).fill().with("253");//filling balance input with new value
+        find("form").last().el("button", containingTextContent("Обновить остаток")).click();
+        isAtProductsPage();
+        //input balance placeholder should contain new value after reload
+        selectProdTable(1);
+        assertThat(prodTable.el("input", withName("balances")).attribute("placeholder")).isEqualTo("253.0");
     }
 
-    private void selectProdTable(int index){
+    @Test
+    public void whenClick_CopyTo_Button_thenProductBalanceUpdatedInTable() {
+        //register->login->add two shops
+        registerAdminUser("useruser", "password");
+        loginUser("useruser", "password");
+        createShop("MyShop1");
+        createShop("MyShop2");
+        goTo(shopsUrl);
+        assertThat(pageSource()).contains("MyShop1", "MyShop2");
+        //opening products page
+        goTo(productsUrl);
+        isAtProductsPage();
+        //filling form with new product
+        selectProdTable(1);//select 1-st existing shop (0 is empty select)
+        find("#type").fillSelect().withText("Товар");//product type (item or service), mandatory
+        find("#name").fill().with("MyProduct");//mandatory input
+        find("#sellingPrice").fill().with("123.45");//mandatory input
+        find("#measurementUnit").fillSelect().withIndex(0);//mandatory select
+        find("#balance").fill().with("150");//mandatory input
+        find("form").first().submit();
+        isAtProductsPage();
+        //copying product from 1-st shop to 2-nd (checkbox+select option with other shop+button)
+        selectProdTable(1);//selecting 1-st shop
+        prodTable.el("input", withName("prods")).click();//clicking checkbox
+        el("select", withName("targetShopId")).click();//clicking select
+        keyboard().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER);//selecting 2-nd shop as target for copying product(0 index - empty option)
+        find("button", containingTextContent("Копировать в")).click();
+        isAtProductsPage();
+        //verifying that 2-nd shop prod's table(which at the moment is last table in dom) contains copied product after reload page
+        selectProdTable(2);//selecting 2-nd shop
+        assertThat(find("table").last().textContent()).contains(
+                "MyProduct",
+                "Товар",
+                "123.45"
+        );
+        assertThat(find("table").last().el("#balances").attribute("placeholder")).isEqualTo("0.0");
+    }
+
+    private void verifyProdTableIsEmpty() {
+        if (prodTable.present()) assertThat(prodTable.textContent()).contains("Нет добавленных товаров");
+    }
+
+    private void selectProdTable(int index) {
         find("#shop").fillSelect().withIndex(index);
     }
 
