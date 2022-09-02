632058f7f953028b1f6cd53e41ed139da7e6740e
pablo <pablo11grande@gmail.com>
2022-09-03 01:34:56 +0500

    added flyway container in docker-compose, removed flyway maven dependency && flyway configuration in application.properties, removed concurrent creating indexes due to pending migrations issue, creted flyway.conf with configurations

--- src/main/resources/db/migration/V1.4__create_index_on_account_username.sql
+++ src/main/resources/db/migration/V1.4__create_index_on_account_username.sql
@@ -1,2 +1,2 @@
-create index concurrently
+create index
     IDX_0256423562bd400a9759ec7f on account (username);
--- src/main/resources/db/migration/V1.5__create_index_on_terminal_tid.sql
+++ src/main/resources/db/migration/V1.5__create_index_on_terminal_tid.sql
@@ -1,2 +1,2 @@
-create index concurrently
+create index
     IDX_947c7e47e3d947719bfe910 on terminal (tid);
